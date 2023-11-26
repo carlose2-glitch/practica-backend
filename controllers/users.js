5a34710dcb087e04acbd94542d5f1e56d31ce33a
Carlos Sanchez <carloseduardosanchezcarrasco@gmail.com>
2023-11-25 22:33:24 -0400

    notification y componente

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,14 @@
-const { request } = require('http');
+//const { request } = require('http');
 
 const usersRouter = require('express').Router();
 
 usersRouter.post('/', async (request, response) => {
 
   const { name, email, password } = request.body;
-  console.log(name, email, password);
+  if(!name || !email || !password){
+
+    return response.status(400).json({ error: 'Todos los espacios son requeridos' });
+  }
 });
 
 module.exports = usersRouter;
