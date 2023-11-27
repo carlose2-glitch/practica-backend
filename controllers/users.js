85b4fb7d75d86994d8322b863121ccbe16d6dac1
Carlos Sanchez <carloseduardosanchezcarrasco@gmail.com>
2023-11-27 13:53:12 -0400

    guardar en mongo

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 //const { request } = require('http');
 
 const usersRouter = require('express').Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 usersRouter.post('/', async (request, response) => {
 
@@ -9,6 +11,20 @@ usersRouter.post('/', async (request, response) => {
 
     return response.status(400).json({ error: 'Todos los espacios son requeridos' });
   }
+
+  const saltRounds = 10;
+
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+
+  const newUser = new User({
+    name,
+    email,
+    passwordHash,
+  });
+
+  const saveUser = await newUser.save();
+  console.log(saveUser);
 });
 
 module.exports = usersRouter;
