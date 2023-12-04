ef3c8650510271f914059296490be12084757ba4
Carlos Sanchez <carloseduardosanchezcarrasco@gmail.com>
2023-12-04 01:27:34 -0400

    crear actualizar borrar mongo

--- controllers/todos.js
+++ controllers/todos.js
@@ -5,11 +5,54 @@ const Todo = require('../models/todo');
 
 todosRouter.get('/', async (request, response) => {
 
-  console.log(request.user);
-
-  //   const todos = await Todo.find({ user: 'adasa' });
-  //   return response.status(200).json(todos);
+  const user = request.user;
+  const todos = await Todo.find({ user: user.id });
+  return response.status(200).json(todos);
 
 });
+todosRouter.post('/', async (request, response) => {
+
+  const user = request.user;
+  const { text } = request.body;
+  const newTodo = new Todo({
+    text,
+    checked: false,
+    user: user._id
+  });
+
+  const savedTodo = await newTodo.save();
+  user.todos = user.todos.concat(savedTodo._id);
+  await user.save();
+  return response.status(201).json(savedTodo);
+
+});
+
+todosRouter.delete('/:id', async (request, response) => {
+
+  const user = request.user;
+
+
+  await Todo.findByIdAndDelete(request.params.id);
+
+  user.todos = user.todos.filter(todo => todo.id !== request.params.id);
+
+  await user.save();
+  return response.status(204);
+
+});
+
+todosRouter.patch('/:id', async (request, response) => {
+
+  const user = request.user;
+
+  const { checked } = request.body;
+
+  await Todo.findByIdAndUpdate(request.params.id, { checked });
+
+  return response.sendStatus(200);
+
+});
+
+
 
 module.exports = todosRouter;
